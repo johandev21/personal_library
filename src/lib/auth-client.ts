01f1d09b06ba364b46c66a290a0cc0f87b71f26f
johandev21 <johandev21@gmail.com>
2025-07-14 01:38:16 -0400

    .

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://personal-library-lfcdeseqi-johandev21s-projects-d27af362.vercel.app"
+    baseURL: "https://personal-library-weld.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient();
