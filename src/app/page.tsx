9a74d22eeac25ae5850fb38df351de0f9a5f60ab
johandev21 <johandev21@gmail.com>
2025-07-13 20:57:27 -0400

    refactor(home): simplify home page to redirect to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import SignoutButton from "@/features/auth/components/signout-button";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session) {
-    redirect("/signin");
-  }
-
-  const userName = session.user.name;
-  const userEmail = session.user.email;
-
-  return (
-    <div className="py-8 px-16">
-      <p>Hello, {userName}</p>
-      <p>Your email is: {userEmail}</p>
-      <SignoutButton />
-    </div>
-  );
+export default function HomePage() {
+  redirect('/dashboard');
 }
