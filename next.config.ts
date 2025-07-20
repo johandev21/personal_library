51681b6e52702893bdbf26438de542f2c63900f8
johandev21 <johandev21@gmail.com>
2025-07-20 15:37:07 -0400

    fix: add PrismaPlugin to webpack configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,25 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https", 
+        protocol: "https",
         hostname: "**",
         port: "",
         pathname: "/**",
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 
